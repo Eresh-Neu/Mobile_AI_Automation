82881f6a036ac2d5393480f53978a5fa2d8bcbb4
Eresh-Neu <ereshagowd.reddy@neudesic.com>
2022-05-04 21:52:28 +0530

    Element changes

--- PageElements/Calculator_digits_Elements.cs
+++ PageElements/Calculator_digits_Elements.cs
@@ -9,10 +9,10 @@ namespace MobileTest.PageElements
     {
        public  string  zero = "com.android.calculator2:id/digit_0";
        public string one = "com.android.calculator2:id/digit_1";
-        public string two = "com.android.calculator2:id/digit_2";
-        public string three = "com.android.calculator2:id/digit_3";
-        public string four = "com.android.calculator2:id/digit_4";
-        public string five = "com.android.calculator2:id/digit_5";
-        public string six = "com.android.calculator2:id/digit_6";
+       public string two = "com.android.calculator2:id/digit_2";
+       public string three = "com.android.calculator2:id/digit_3";
+       public string four = "com.android.calculator2:id/digit_4";
+       public string five = "com.android.calculator2:id/digit_5";
+       public string six = "com.android.calculator2:id/digit_6";
     }
 }
