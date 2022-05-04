ad1752f5e2fa24bbea14584bce7e37387850edd2
Eresh-Neu <ereshagowd.reddy@neudesic.com>
2022-05-04 20:47:23 +0530

    changes in elements

--- PageElements/Calculator_digits_Elements.cs
+++ PageElements/Calculator_digits_Elements.cs
@@ -8,5 +8,6 @@ namespace MobileTest.PageElements
     public class Calculator_digits_Elements
     {
        public  string  zero = "com.android.calculator2:id/digit_0";
+       public string one = "com.android.calculator2:id/digit_1";
     }
 }
